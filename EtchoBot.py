33122e0b6e0a91d63d57377aeade673f2c6099d8
KeithEsc <91182126+KeithEsc@users.noreply.github.com>
2021-09-22 01:24:41 -0600

    Add files via upload

---
+++ EtchoBot.py
@@ -0,0 +1,71 @@
+import discord
+import youtube_dl
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello, I am EtchoBot. Use the commands !join, !play, and !stop to play music from Youtube.")
+
+
+@client.command(pass_context=True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You need to be in a voice channel to use this command.")
+
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("I left the voice channel.")
+    else:
+        await ctx.send("I am not in a voice channel.")
+
+
+@client.command(pass_context=True)
+async def stop(ctx):
+    voice=discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+@client.command(pass_context=True)
+async def play(ctx, url : str):
+    song_there=os.path.isfile("song.webm")
+    try:
+        if song_there:
+            os.remove("song.webm")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use !stop command.")
+
+    voiceChannel=discord.utils.get(ctx.guild.voice_channels)
+    voice=discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': '249/250/251',
+        '': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'webm',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".webm"):
+            os.rename(file, "song.webm")
+    voice.play(discord.FFmpegPCMAudio("song.webm"))
+
+client.run('Mjc0Njk2MzY2MzQ4MDQyMjQw.WIvk7Q.h3Ie8oPDmrfTXlhXHuSZARbVZa0')
